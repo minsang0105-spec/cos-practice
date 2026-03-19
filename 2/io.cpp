0f0127a8312af9a6bc6be2f2b67b4788bc8fc552
minsang0105-spec <minsang0105@kentech.ac.kr>
2026-03-19 01:28:18 +0000

    --all

---
+++ 2/io.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[])
+{
+	int x{};
+	cout << "Enter a number: ";
+	cin >> x;
+	cout << "You entered " << x << std::endl;
+	return 0;
+}
